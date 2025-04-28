78a4a3a2787098a465dd3cbc6ff594dd9f8a35cb
Julius <s_boeger24@stud.hwr-berlin.de>
2025-04-28 16:31:48 +0200

    clean data

--- utils.py
+++ utils.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
 def load_data():
-    
+    """Load the Titanic dataset and filter"""
     df = pd.read_csv('data/titanic.csv')
     df = df[df["sex"] == "male"]
+    return df
+
+def clean_data(df):
+    """
+    Clean the data by dropping unnecessary columns and renaming others."""
+    df = df.dropna()
+    
+    df['embarked'] = df["embarked"].str.lower()
+    
     return df
